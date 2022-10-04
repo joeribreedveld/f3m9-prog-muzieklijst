fc757cb8a050b3aa4bca06ef0767b397f61f5c9b
Duck1s <32653@ma-web.nl>
2022-10-04 13:26:22 +0200

    feat(muzieklijst): add input field and functionality

--- components/Muzieklijst.js
+++ components/Muzieklijst.js
@@ -10,6 +10,31 @@ class Muzieklijst extends React.Component {
 		}
 	}
 
+	inputPressedEnter = (input) => {
+		let toBeAdded = [
+			{
+				name: input,
+				done: false,
+				id: 100,
+			},
+		]
+		let mergedArray = this.state.tasks.concat(toBeAdded)
+		this.setState({
+			tasks: mergedArray,
+		})
+	}
+
+	update = (e) => {
+		this.setState({ inputValue: e.target.value })
+	}
+
+	enter = (e) => {
+		if (e.keyCode === 13) {
+			console.log("enter")
+			this.inputPressedEnter(this.state.inputValue)
+		}
+	}
+
 	componentDidMount() {
 		this.setState({
 			tasks: [],
@@ -17,10 +42,20 @@ class Muzieklijst extends React.Component {
 	}
 
 	render() {
+		const items = this.state.tasks.map((task) => {
+			return (
+				<>
+					<p>{task.name}</p>
+				</>
+			)
+		})
+
 		return (
 			<>
 				<h1 className='text-2xl'>Muzieklijst</h1>
 				<p>{this.props.genre}</p>
+				<ul>{items}</ul>
+				<input onChange={this.update} onKeyDown={this.enter} value={this.state.inputValue} className='input' type='text' placeholder='Nummer' />
 			</>
 		)
 	}
