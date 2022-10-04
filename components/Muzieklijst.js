34dd18c268beeaefa520847b940c1f30cca7a050
Duck1s <32653@ma-web.nl>
2022-10-04 14:10:50 +0200

    feat(muzieklijst): add all functions and state and everything

--- components/Muzieklijst.js
+++ components/Muzieklijst.js
@@ -6,52 +6,75 @@ class Muzieklijst extends React.Component {
 	constructor(props) {
 		super(props)
 		this.state = {
-			tasks: [],
+			nummers: [],
 		}
 	}
 
 	// Enter functionality
-	inputPressedEnter = (input) => {
+	inputPressedEnter = (nummer, artiest) => {
 		let toBeAdded = [
 			{
-				name: input,
+				nummer: nummer,
+				artiest: artiest,
 				done: false,
 				id: 100,
 			},
 		]
-		let mergedArray = this.state.tasks.concat(toBeAdded)
+		let mergedArray = this.state.nummers.concat(toBeAdded)
 		this.setState({
-			tasks: mergedArray,
+			nummers: mergedArray,
+			nummerValue: "",
+			artiestValue: "",
 		})
 	}
 
 	// Update
-	update = (e) => {
-		this.setState({ inputValue: e.target.value })
+	updateArtiest = (e) => {
+		this.setState({ artiestValue: e.target.value })
+		this.setState({ artiestValue: e.target.value })
+	}
+
+	updateNummer = (e) => {
+		this.setState({ nummerValue: e.target.value })
+		this.setState({ nummerValue: e.target.value })
 	}
 
 	// Enter
 	enter = (e) => {
-		if (e.keyCode === 13) {
-			console.log("enter")
-			this.inputPressedEnter(this.state.inputValue)
+		if (this.state.nummerValue && this.state.artiestValue && Object.keys(this.state.nummerValue).length !== 0 && Object.keys(this.state.artiestValue).length !== 0) {
+			if (e.keyCode === 13) {
+				console.log("enter")
+				this.inputPressedEnter(this.state.nummerValue, this.state.artiestValue)
+			}
 		}
+	}
+
+	enterButton = (e) => {
+		if (this.state.nummerValue && this.state.artiestValue && Object.keys(this.state.nummerValue).length !== 0 && Object.keys(this.state.artiestValue).length !== 0) {
+			console.log("enter")
+			this.inputPressedEnter(this.state.nummerValue, this.state.artiestValue)
+		}
+	}
+
+	cancelButton = (e) => {
+		this.setState({ nummerValue: "", artiestValue: "" })
 	}
 
 	// Mount
 	componentDidMount() {
 		this.setState({
-			tasks: [],
+			nummers: [],
 		})
 	}
 
 	// Render
 	render() {
-		const items = this.state.tasks.map((task) => {
+		const items = this.state.nummers.map((nummer) => {
 			return (
 				<>
-					<li>
-						<p>{task.name}</p>
+					<li className=''>
+						<p>Nummer: {nummer.nummer}</p>
+						<p>Artiest: {nummer.artiest}</p>
 					</li>
 				</>
 			)
@@ -59,10 +82,22 @@ class Muzieklijst extends React.Component {
 
 		return (
 			<>
-				<h1 className='text-2xl'>Muzieklijst</h1>
-				<p>{this.props.genre}</p>
-				<ul>{items}</ul>
-				<input onChange={this.update} onKeyDown={this.enter} value={this.state.inputValue} className='input' type='text' placeholder='Nummer' />
+				<article>
+					<h2 className='text-2xl mb-4 font-medium'>{this.props.genre}</h2>
+					<ul className='flex flex-col gap-4'>{items}</ul>
+					<ul className='flex gap-8 mb-2 mt-2'>
+						<input onChange={this.updateNummer} onKeyDown={this.enter} value={this.state.nummerValue} className='bg-[#f8f8f8] text-black p-4 mt-4' type='text' placeholder='Nummer' />
+						<input onChange={this.updateArtiest} onKeyDown={this.enter} value={this.state.artiestValue} className='bg-[#f8f8f8] text-black p-4 mt-4' type='text' placeholder='Artiest' />
+					</ul>
+					<ul className='flex gap-8'>
+						<button className='bg-[#b1ffb1] hover:bg-[#99e899] h-full w-1/2 py-4 px-8 mt-4 text-white font-bold transition-all transition-duration-200' onClick={this.enterButton}>
+							Enter
+						</button>
+						<button className='bg-[#ff9999] hover:bg-[#e27e7e] h-full w-1/2 py-4 px-8 mt-4 text-white font-bold transition-all transition-duration-200' onClick={this.cancelButton}>
+							Cancel
+						</button>
+					</ul>
+				</article>
 			</>
 		)
 	}
--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,29 @@ import Muzieklijst from "../components/Muzieklijst"
 function Home() {
 	return (
 		<>
-			<h1 className='text-3xl'>Hello, World!</h1>
-			<Muzieklijst genre='Genre Titel' />
+			<section className='p-8 md:p-16 lg:p-32'>
+				<h1 className='text-3xl mb-16'>Mijn Muzieklijst</h1>
+				<ul className='flex flex-wrap gap-x-8 gap-y-16'>
+					<li className='shadow-xl p-12'>
+						<Muzieklijst genre='Rock' />
+					</li>
+					<li className='shadow-xl p-12'>
+						<Muzieklijst genre='Jazz' />
+					</li>
+					<li className='shadow-xl p-16'>
+						<Muzieklijst genre='House' />
+					</li>
+					<li className='shadow-xl p-12'>
+						<Muzieklijst genre='Blues' />
+					</li>
+					<li className='shadow-xl p-12'>
+						<Muzieklijst genre='Pop' />
+					</li>
+					<li className='shadow-xl p-12'>
+						<Muzieklijst genre='Hip Hop' />
+					</li>
+				</ul>
+			</section>
 		</>
 	)
 }
